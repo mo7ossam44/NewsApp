3b1b7d1eee3d46240fe626d9e4815c0793dfc722
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-19 23:21:57 +0300

    add custom scroll view

--- lib/Screens/home_page.dart
+++ lib/Screens/home_page.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:news_cloud/Widgets/categories_list_view.dart';
+import 'package:news_cloud/Widgets/news_container_listview.dart';
+import 'package:news_cloud/Widgets/rich_text.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
@@ -7,29 +9,16 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        centerTitle: true,
-        title: RichText(
-          text: TextSpan(
-            text: 'News', // Default text
-            style: TextStyle(
-              color: Colors.black,
-              fontSize: 25,
-              fontWeight: FontWeight.bold,
-            ),
-            children: [
-              TextSpan(
-                text: 'Cloud',
-                style: TextStyle(
-                  fontWeight: FontWeight.bold,
-                  color: Colors.orange,
-                ),
-              ),
-            ],
-          ),
-        ),
+      appBar: AppBar(centerTitle: true, title: RichTextWidget()),
+      body: CustomScrollView(
+        physics: BouncingScrollPhysics(),
+        slivers: [
+          //? -> you must used widget<Sliver> only here
+          SliverToBoxAdapter(child: CategoriesListView()),
+          const SliverToBoxAdapter(child: SizedBox(height: 30)),
+          const NewsContainerListView(),
+        ],
       ),
-      body: CategoriesListView(),
     );
   }
 }
