585259b268062ddb08eaaf090caf6492be5577e7
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-21 16:25:32 +0300

    create article model

---
+++ lib/Models/article_model.dart
@@ -0,0 +1,11 @@
+class ArticleModel {
+  final String? title;
+  final String? subtitle;
+  final String? urlToImage;
+
+  ArticleModel({
+    required this.title,
+    required this.subtitle,
+    required this.urlToImage,
+  });
+}
