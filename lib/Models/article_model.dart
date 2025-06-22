d8be48e0fac840d2640e624b1c627fec605d8ee8
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-22 18:24:02 +0300

    add one more attr in arrticle model

--- lib/Models/article_model.dart
+++ lib/Models/article_model.dart
@@ -1,10 +1,12 @@
 class ArticleModel {
   final String? title;
+  final String? author;
   final String? subtitle;
   final String? urlToImage;
 
   ArticleModel({
     required this.title,
+    required this.author,
     required this.subtitle,
     required this.urlToImage,
   });
