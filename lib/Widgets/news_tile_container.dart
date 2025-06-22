f2b9c8d355e677fe292913494ae1050fa84b786b
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-22 18:35:29 +0300

    add a gesture detector widget

--- lib/Widgets/news_tile_container.dart
+++ lib/Widgets/news_tile_container.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:news_cloud/Models/article_model.dart';
+import 'package:news_cloud/Screens/single_new.dart';
 
 class NewsTileContainer extends StatelessWidget {
   const NewsTileContainer({super.key, required this.articleModel});
@@ -8,47 +9,65 @@ class NewsTileContainer extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: [
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 20),
-          child: ClipRRect(
-            borderRadius: BorderRadius.circular(20),
-            child: articleModel.urlToImage != null
-                ? Image.network(
-                    articleModel.urlToImage!,
-                    height: 200,
-                    width: double.infinity,
-                    fit: BoxFit.cover,
-                  )
-                : SizedBox(),
-          ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
-          child: Text(
-            articleModel.title ?? '',
-            style: TextStyle(
-              overflow: TextOverflow.ellipsis,
-              fontSize: 20,
-              fontWeight: FontWeight.bold,
+    return GestureDetector(
+      onTap: () => Navigator.of(
+        context,
+      ).push(MaterialPageRoute(builder: (context) => SingleNew(articleModel: articleModel,))),
+      child: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 20),
+            child: ClipRRect(
+              borderRadius: BorderRadius.circular(20),
+              child: articleModel.urlToImage != null
+                  ? Image.network(
+                      articleModel.urlToImage!,
+                      height: 200,
+                      width: double.infinity,
+                      fit: BoxFit.cover,
+                    )
+                  : Container(
+                      height: 200,
+                      color: Colors.orange,
+                      child: Center(
+                        child: Text(
+                          'There is no photo',
+                          style: TextStyle(
+                            fontWeight: FontWeight.bold,
+                            fontSize: 25,
+                          ),
+                        ),
+                      ),
+                    ),
             ),
           ),
-        ),
-        Padding(
-          padding: const EdgeInsets.symmetric(horizontal: 30),
-          child: Text(
-            maxLines: 2,
-            articleModel.subtitle ?? '',
-            style: TextStyle(
-              overflow: TextOverflow.ellipsis,
-              fontSize: 17,
-              color: Colors.grey.shade700,
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30, vertical: 5),
+            child: Text(
+              articleModel.title ?? '',
+              style: TextStyle(
+                overflow: TextOverflow.ellipsis,
+                fontSize: 20,
+                fontWeight: FontWeight.bold,
+              ),
             ),
           ),
-        ),
-        SizedBox(height: 20),
-      ],
+          Padding(
+            padding: const EdgeInsets.symmetric(horizontal: 30),
+            child: Text(
+              maxLines: 2,
+              articleModel.subtitle ??
+                  'There is no subtitle in this new tile only the title',
+              style: TextStyle(
+                overflow: TextOverflow.ellipsis,
+                fontSize: 17,
+                color: Colors.grey.shade700,
+              ),
+            ),
+          ),
+          SizedBox(height: 20),
+        ],
+      ),
     );
   }
 }
