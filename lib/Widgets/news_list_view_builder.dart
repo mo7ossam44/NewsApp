49ca7eac4c4b19c7bfa7098026e03b3ce91e5901
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-22 18:35:58 +0300

    add a news list view builder

---
+++ lib/Widgets/news_list_view_builder.dart
@@ -0,0 +1,49 @@
+
+import 'package:flutter/material.dart';
+import 'package:news_cloud/Models/article_model.dart';
+import 'package:news_cloud/Services/news_service.dart';
+import 'package:news_cloud/Widgets/news_container_listview.dart';
+
+class NewsListViewBuilder extends StatefulWidget {
+  const NewsListViewBuilder({super.key});
+
+  @override
+  State<NewsListViewBuilder> createState() => _NewsListViewBuilderState();
+}
+
+class _NewsListViewBuilderState extends State<NewsListViewBuilder> {
+  bool isLoading = true;
+  List<ArticleModel> articlesNewsContainerList = [];
+
+  //! First Level in STF LisfeCycle
+  //! init state called only one time
+  //! cannot make init state asynchrons method
+  @override
+  void initState() {
+    super.initState();
+    newsServiceMethod();
+  }
+
+  Future<void> newsServiceMethod() async {
+    articlesNewsContainerList = await NewsService().getNews();
+    isLoading = false;
+    setState(() {});
+  }
+
+
+  //! Second Level in STF LisfeCycle
+  //! cannot call any method in a build method to avoid rebuild alot of times
+  @override
+  Widget build(BuildContext context) {
+    return isLoading
+        ? SliverFillRemaining(
+            hasScrollBody: false,
+            child: Center(
+              child: CircularProgressIndicator(color: Colors.amber),
+            ),
+          )
+        : NewsContainerListView(
+            articlesNewsContainerList: articlesNewsContainerList,
+          );
+  }
+}
