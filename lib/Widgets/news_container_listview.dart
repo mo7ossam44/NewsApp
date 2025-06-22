bfedc416460ebc65561b6efc444f7bc8872a91ec
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-22 18:37:11 +0300

    make the page make only one thing

--- lib/Widgets/news_container_listview.dart
+++ lib/Widgets/news_container_listview.dart
@@ -1,58 +1,20 @@
 import 'package:flutter/material.dart';
 import 'package:news_cloud/Models/article_model.dart';
-import 'package:news_cloud/Services/news_service.dart';
 import 'package:news_cloud/Widgets/news_tile_container.dart';
 
-class NewsContainerListView extends StatefulWidget {
-  NewsContainerListView({super.key});
+class NewsContainerListView extends StatelessWidget {
+  NewsContainerListView({super.key, required this.articlesNewsContainerList});
 
-  @override
-  State<NewsContainerListView> createState() => _NewsContainerListViewState();
-}
+  final List<ArticleModel> articlesNewsContainerList;
 
-class _NewsContainerListViewState extends State<NewsContainerListView> {
-  bool isLoading = true;
-  List<ArticleModel> articlesNewsContainerList = [];
-
-  //! First Level in STF LisfeCycle
-  //! init state called only one time
-  //! cannot make init state asynchrons method
-  @override
-  void initState() {
-    super.initState();
-    newsServiceMethod();
-  }
-
-  Future<void> newsServiceMethod() async {
-    articlesNewsContainerList = await NewsService().getNews();
-    isLoading = false;
-    setState(() {});
-  }
-
-  //! Second Level in STF LisfeCycle
-  //! cannot call any method in a build method to avoid rebuild alot of times
   @override
   Widget build(BuildContext context) {
-    return isLoading
-        ? SliverFillRemaining(
-            hasScrollBody: false,
-            child: Center(
-              child: CircularProgressIndicator(color: Colors.amber),
-            ),
-          )
-        : SliverList(
-            delegate: SliverChildBuilderDelegate(
-              childCount: articlesNewsContainerList.length,
-              (context, index) => NewsTileContainer(
-                articleModel: articlesNewsContainerList[index],
-              ),
-            ),
-          );
-  }
-
-  //! Last Level in STF LisfeCycle
-  @override
-  void dispose() {
-    super.dispose();
+    return SliverList(
+      delegate: SliverChildBuilderDelegate(
+        childCount: articlesNewsContainerList.length,
+        (context, index) =>
+            NewsTileContainer(articleModel: articlesNewsContainerList[index]),
+      ),
+    );
   }
 }
