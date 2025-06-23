0e2f4e96d010b8f5b5bae756ddab446a9e0d1ed5
Mohamed Hossam <m.mansi2hossam@gmail>
2025-06-23 19:58:30 +0300

    add url attr and factory constractor

--- lib/Models/article_model.dart
+++ lib/Models/article_model.dart
@@ -1,13 +1,23 @@
 class ArticleModel {
+  final String? url;
   final String? title;
   final String? author;
   final String? subtitle;
   final String? urlToImage;
-
   ArticleModel({
+    required this.url,
     required this.title,
     required this.author,
     required this.subtitle,
     required this.urlToImage,
   });
+  factory ArticleModel.fromJson(Map<String, dynamic> json) {
+    return ArticleModel(
+      url: json['url'],
+      title: json['title'],
+      author: json['author'],
+      subtitle: json['description'],
+      urlToImage: json['urlToImage'],
+    );
+  }
 }
